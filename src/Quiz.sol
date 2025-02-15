d85e0306ca1e891a90e31abd75ac83784f72c1d0
leech1996 <leech1996@gmail.com>
2025-02-15 20:30:45 +0900

    solved

--- src/Quiz.sol
+++ src/Quiz.sol
@@ -9,7 +9,10 @@ contract Quiz{
       uint min_bet;
       uint max_bet;
    }
-    
+   
+    mapping(uint => Quiz_item) private quizzes;
+    uint private quizNum = 0;
+
     mapping(address => uint256)[] public bets;
     uint public vault_balance;
 
@@ -24,24 +27,60 @@ contract Quiz{
     }
 
     function addQuiz(Quiz_item memory q) public {
+        require(msg.sender != 0x0000000000000000000000000000000000000001, "Go away.");
+        quizzes[q.id] = q;
+        quizNum = q.id;
+        bets.push();
     }
 
     function getAnswer(uint quizId) public view returns (string memory){
+        Quiz_item memory q;
+        q = quizzes[quizId];
+        return q.answer;
     }
 
     function getQuiz(uint quizId) public view returns (Quiz_item memory) {
+        Quiz_item memory q;
+        q = quizzes[quizId];
+        q.answer = "";
+        return q;
     }
 
     function getQuizNum() public view returns (uint){
+        return quizNum;
     }
     
     function betToPlay(uint quizId) public payable {
+        Quiz_item memory q;
+        q = quizzes[quizId];
+        require(msg.value >= q.min_bet, "bet more");
+        require(msg.value <= q.max_bet, "bet less");
+        bets[quizId-1][msg.sender] += msg.value;
     }
 
     function solveQuiz(uint quizId, string memory ans) public returns (bool) {
+        Quiz_item memory q;
+        q = quizzes[quizId];
+        if (keccak256(abi.encodePacked(ans)) == keccak256(abi.encodePacked(q.answer))){
+            bets[quizId-1][msg.sender] *= 2;
+            return true;
+        }
+        else{
+            vault_balance += bets[quizId-1][msg.sender];
+            bets[quizId-1][msg.sender] = 0;
+            return false;
+        }
     }
 
     function claim() public {
+        uint256 sum = 0;
+        for(uint256 i=0; i<quizNum; i++){
+            sum += bets[i][msg.sender];
+        }
+        (bool s, bytes memory r) = msg.sender.call{value: sum}("");
+        require(s, "claim failed");
     }
 
+    receive() external payable {
+    }
 }
